a8aa20bc97d0e4f9ea0be859252516663d7a22b2
WiktorPieklik <wiktor.pieklik@icloud.com>
2023-05-05 13:55:13 +0200

    feat: init repo

---
+++ README.md
@@ -0,0 +1,9 @@
+# Detecting fake news using DistilBERT
+
+## Setup
+1. Create venv with your favourite tool
+2. Activate it
+3. Run 
+```bash
+   python install.py
+```
---
+++ install.py
@@ -0,0 +1,11 @@
+from sys import executable
+from subprocess import check_call
+
+dependencies = [
+
+]
+
+if __name__ == "__main__":
+    check_call([executable, "-m", "pip", "install", "--upgrade", "pip", "setuptools", "wheel"])
+    for dependency in dependencies:
+        check_call([executable, "-m", "pip", "install", dependency])
