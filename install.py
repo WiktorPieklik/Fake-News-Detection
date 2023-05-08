017f94c710e061029369004369a7617b8c2340b7
WiktorPieklik <wiktor.pieklik@icloud.com>
2023-05-08 18:54:46 +0200

    chore: dependencies

--- install.py
+++ install.py
@@ -1,11 +1,27 @@
-from sys import executable
+from typing import List
+from sys import executable, platform
 from subprocess import check_call
 
 dependencies = [
-
+    'jupyter',
+    'transformers',
+    'pandas',
+    'numpy'
 ]
+
+darwin_dependencies = [
+    'tensorflow-macos',
+    'tensorflow-metal'
+]
+
+
+def install_deps(deps: List[str]) -> None:
+    for dependency in deps:
+        check_call([executable, "-m", "pip", "install", dependency])
+
 
 if __name__ == "__main__":
     check_call([executable, "-m", "pip", "install", "--upgrade", "pip", "setuptools", "wheel"])
-    for dependency in dependencies:
-        check_call([executable, "-m", "pip", "install", dependency])
+    install_deps(dependencies)
+    if platform == 'darwin':
+        install_deps(darwin_dependencies)
