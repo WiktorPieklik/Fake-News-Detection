b590cb13f2968e9710df11e9be77f51ee6b6c2d4
WiktorPieklik <wiktor.pieklik@icloud.com>
2023-05-09 23:46:53 +0200

    chore(dataset): setup & help scripts

---
+++ raw/prepare_dataset.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+fake='rumours'
+dst_fake="dataset/fake"
+dst_true="dataset/true"
+
+openssl enc -d -aes256 -in dataset.enc -pbkdf2 -pass file:dataset.key | tar -xzf -
+dirs=$(find raw_dataset -mindepth 2 -maxdepth 2 -type d)
+
+mkdir -p $dst_fake
+mkdir -p $dst_true
+
+for dir in $dirs; do
+  type=$(basename $dir)
+  if [ $type == $fake ]; then
+    mv $dir/* $dst_fake
+  else
+    mv $dir/* $dst_true
+  fi
+done
+
+rm -rf raw_dataset
